2428830ebab3f6589d1b0e060e93ac2578887e0f
AnushkaGhei <anushkaghei@gmail.com>
2021-12-14 13:15:29 +0530

    part1

--- part1.py
+++ part1.py
@@ -1,5 +1,16 @@
-import math
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
 
-a= 1
-b= 2
-print(a +b)
+class Face_Recognition_System:
+    def _init_(self,root):
+        self.root=root
+        self.root.geometry("1500x800")
+        self.root.title("Student Attendance Management System")
+
+
+
+if _name_=="_main_":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
